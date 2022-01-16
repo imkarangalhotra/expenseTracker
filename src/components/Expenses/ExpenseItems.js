8d26afb59079266dcc1ecab51f8ce23537aa20bd
imkaran <karangalhotra0001@gmail.com>
2022-01-17 02:03:16 +0530

    static app with fixed values created

---
+++ src/components/Expenses/ExpenseItems.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+function ExpenseItems(props) {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+      </div>
+      <div className='expense-item__price'>₹{props.amount}</div>
+    </Card>
+  );
+}
+
+export default ExpenseItems;
